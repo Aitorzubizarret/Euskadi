e0a1b4598d1e3e58c4a50cafd3b93a47272fd9da
Aitor <aitorzubizarreta@yahoo.es>
2020-11-03 19:39:39 +0100

    Updated 'TownDetailViewController' and its xib file. Added an image and a scrollview to the view.

--- Euskadi/Controllers/TownDetailViewController.swift
+++ Euskadi/Controllers/TownDetailViewController.swift
@@ -11,23 +11,50 @@ import UIKit
 class TownDetailViewController: UIViewController {
 
     // MARK: - UI
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var detailImage: UIImageView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var contentViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var townNameLabel: UILabel!
     
     // MARK: - Properties
+    
     weak var coordinator: MainCoordinator?
     public var town: Town?
     
     // MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.setContenViewHeight()
+        
+        // Checks if the VC received a Town.
         if let thisTown = self.town {
+            self.setTownImage()
             self.setTownName(name: thisTown.name)
         }
     }
     
     ///
-    /// Sets a value (Town name) to the label 'townNameLabel' from a property.
+    /// Sets the height of the contentView and thus the height of the ScrollView.
+    ///
+    private func setContenViewHeight() {
+        self.contentViewHeightConstraint.constant = 1300
+        self.view.layoutIfNeeded()
+    }
+    
+    ///
+    /// WILL Set the town image.
+    ///
+    private func setTownImage() {
+        self.detailImage.contentMode = .scaleAspectFill
+        self.detailImage.image = UIImage(named: "default-town")
+    }
+    
+    ///
+    /// Sets a value (Town name) to the label 'townNameLabel'.
     ///
     private func setTownName(name: String) {
         self.townNameLabel.text = name
